364e63741188e5890e2a331b0d06c83ddeb832d4
yodaaa <yodaaa@macbookproyodaaa.local>
2017-10-30 13:38:57 +0900

    Initial Commit

---
+++ KudanSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KudanSample
+//
+//  Created by yodaaa on 2017/10/30.
+//  Copyright © 2017年 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
